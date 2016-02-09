53ceab2c00750c89f1f0f9a67a825de9d3c531fb
Xelen93 <253077>
2016-02-09 03:02:53 +0300

    Lesson 2:  Added router.
    Added links to local Framework,
    added router,
    created 2 controllers.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-angular.module('bestApp', [])
-.directive('mySum', function() {
- return {
-  restrict: 'E',
-  template:
-  '<input type="number" ng-model="number_one">' +
+angular.module('bestApp', ['ngRoute'])
+
+ .controller('MainController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ })
+
+ .controller('ChapterController', function($scope, $routeParams) {
+     $scope.name = "ChapterController";
+     $scope.params = $routeParams;
+ })
+
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/:bookId/x/:chapterId', {
+    template:
+  '<input type="text" ng-model="params.bookId" value="{{params.bookId}}">' +
 	'<span>+</span>' +
-	'<input type="number" ng-model="number_two">' +
+	'<input type="text" ng-model="params.chapterId" value="{{params.chapterId}}">' +
 	'<span>=</span>' +
-	'<input type="number" value="{{number_one + number_two}}" disabled>'
-  }
+	'<input type="text" value="{{(params.bookId-0) + (params.chapterId-0)}}" disabled>',
+	controller: 'ChapterController'
+  })
+  .when('/', {
+   template:
+  '<input type="text" ng-model="params.bookId" value="{{params.bookId}}">' +
+  '<span>+</span>' +
+  '<input type="text" ng-model="params.chapterId" value="{{params.chapterId}}">' +
+  '<span>=</span>' +
+  '<input type="text" value="{{(params.bookId-0) + (params.chapterId-0)}}" disabled>',
+  controller: 'ChapterController'
+  });
 });
